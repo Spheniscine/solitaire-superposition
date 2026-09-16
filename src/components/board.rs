63135adc028d6e24dc00af1fcd79c368d0756e3b
Spheniscine <spheniscine@gmail.com>
2026-09-16 17:30:49 +0800

    add animations

--- src/components/board.rs
+++ src/components/board.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 use glam::Vec2;
 use math_macro::math;
 
-use crate::{components::{CARD_BORDER_RADIUS_RATIO, CARD_HEIGHT_RATIO, CardComponent, CardFrame, EMOJI_MAP, SkinTrait, rem}, game::{AnimationKey, Board, BoardPos, Card, DepotRole, NUM_DEPOTS, Skin, Suit}};
+use crate::{components::{CARD_BORDER_RADIUS_RATIO, CARD_HEIGHT_RATIO, CardComponent, CardFrame, EMOJI_MAP, Movement, SkinTrait, rem}, game::{AnimationAct, AnimationKey, Board, BoardPos, Card, DepotRole, NUM_DEPOTS, Skin, Suit}};
 
 // symbol used for superpositor
 #[component]
@@ -126,6 +126,59 @@ pub fn BoardComponent(
         }
     };
 
+    let moving_card = |p1: Vec2, p2: Vec2, card: Card| rsx! {
+        Movement {
+            src_translate_vec: p1 - p2,
+            CardComponent {
+                position: p2,
+                width: card_width,
+                card: card,
+                skin,
+            }
+        }
+    };
+
+    let anims = board.animation_acts.iter().enumerate().map(|(i, act)| {
+        match act {
+            AnimationAct::Move { cards, pos1, pos2 } => {
+                let mut pos1 = *pos1;
+                let mut pos2 = *pos2;
+
+                let nodes = cards.iter().map(move |card| {
+                    let p1 = get_pos(pos1.depot_index, pos1.card_index);
+                    let p2 = get_pos(pos2.depot_index, pos2.card_index);
+                    let res = moving_card(p1, p2, *card);
+                    pos1.card_index += 1;
+                    pos2.card_index += 1;
+                    res
+                });
+
+                rsx! {
+                    Fragment {
+                        key: "{animation_key},{i}", // needed to force remounts, so animations don't get "stale" and refuse to replay
+                        {nodes}
+                    }
+                }
+            },
+            AnimationAct::Split { card, pos, undo } => {
+                let p1 = get_pos(pos.depot_index, pos.card_index);
+                let p2 = get_pos(DepotRole::Superpositor.id(0), 0);
+                let node = if !undo {
+                    moving_card(p1, p2, *card)
+                } else {
+                    moving_card(p2, p1, *card)
+                };
+
+                rsx! {
+                    Fragment {
+                        key: "{animation_key},{i}", // needed to force remounts, so animations don't get "stale" and refuse to replay
+                        {node}
+                    }
+                }
+            },
+        }
+    });
+
     rsx! {
         div {
             position: "absolute",
@@ -179,6 +232,24 @@ pub fn BoardComponent(
                             oncontextmenu.call(BoardPos::new(depot, i))
                         },
                     }
+                }
+            }
+
+            {anims}
+
+            if is_won {
+                div {
+                    position: "absolute",
+                    top: rem(25.),
+                    left: rem(17.5),
+                    width: rem(59.),
+                    background_color: "#505",
+                    padding: rem(3.),
+                    color: "#fff",
+                    font_size: rem(7.),
+                    border_radius: rem(2.),
+                    text_align: "center",
+                    "YOU WIN!",
                 }
             }
         }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -10,8 +10,8 @@ mod skin;
 mod board;
 pub use board::*;
 
-// mod anim;
-// pub use anim::*;
+mod anim;
+pub use anim::*;
 
 mod hero;
 pub use hero::*;
