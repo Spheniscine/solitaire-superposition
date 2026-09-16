e267fc65fedbc6cd2e14731e806894e16daa0311
Spheniscine <spheniscine@gmail.com>
2026-09-16 22:02:38 +0800

    remove unnecessary checks

--- src/game/game.rs
+++ src/game/game.rs
@@ -265,22 +265,14 @@ impl GameState {
 
         if !self.can_select(pos) { return; } // needed, or illegal stacks can still be moved this way!
 
-        let depot = &self.board.depots[pos.depot_index];
-        let num_moved = depot.len() - pos.card_index;
-        if num_moved != 1 { return; }
-
         self.try_sort(pos);
     }
 
     pub fn oncontextmenu(&mut self, pos: BoardPos) {
         if self.is_busy() { return; }
         if self.is_over() { return; }
-        
-        if !self.can_select(pos) { return; } // needed, or illegal stacks can still be moved this way!
 
-        let depot = &self.board.depots[pos.depot_index];
-        let num_moved = depot.len() - pos.card_index;
-        if num_moved != 1 { return; }
+        if !self.can_select(pos) { return; } // needed, or illegal stacks can still be moved this way!
 
         self.move_intent(pos, self.board.top_pos(DepotRole::Superpositor.id(0)));
     }
